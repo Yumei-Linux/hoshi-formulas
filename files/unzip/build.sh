e004b55dc621cba4a5c93daf9cc8dd433cc39abc
Gabriel Guerra <gfranklings@gmail.com>
2024-02-19 16:11:49 -0400

    feat: packaging unzip

---
+++ files/unzip/build.sh
@@ -0,0 +1,15 @@
+downloads() {
+  tar xpvf $downloads/unzip60.tar.gz
+  mv $downloads/unzip-6.0-consolidated_fixes-1.patch $downloads/unzip60
+}
+
+build() {
+  cd unzip60
+  patch -Np1 -i ../unzip-6.0-consolidated_fixes-1.patch
+
+  make -f unix/Makefile generic -j`nproc`
+
+  make prefix=$dest/usr MANDIR=$dest/usr/share/man/man1 \
+    -f unix/Makefile install
+}
+
