8133232fab40bac22ec3b3f1e08ac045bfa449a8
Gabriel Guerra <gfranklings@gmail.com>
2024-02-20 16:23:27 -0400

    chore: packaging lm-sensors dependencies

---
+++ sys/m4/build.sh
@@ -0,0 +1,14 @@
+downloads() {
+  tar xpvf $downloads/m4-latest.tar.xz
+}
+
+build() {
+  cd m4-latest
+
+  sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
+  echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
+
+  ./configure --prefix=$dest/usr
+
+  make -j`nproc` && make install
+}
