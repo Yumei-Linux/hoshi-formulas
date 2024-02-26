68280090a1fb5862f274c8e6a0895641203c1ee5
Gabriel Guerra <gfranklings@gmail.com>
2024-02-26 14:20:24 -0400

    chore: register.sh: added a way to unregister packages

--- register.sh
+++ register.sh
@@ -11,9 +11,9 @@ if [[ "$dirname" == "" ]] || ! test -d "$dirname"; then
   error "Invalid directory to register"
 fi
 
-info "Registering a local copy of ${COLOR_GREEN}${dirname}${COLOR_RESET} at the formulas dir"
-
 register_copy() {
+  info "Registering a local copy of ${COLOR_GREEN}${dirname}${COLOR_RESET} at the formulas dir"
+
   category=$(echo $1 | awk -F'/' '{print $(NF-1)}')
   package=$(echo $1 | awk -F'/' '{print $NF}')
 
@@ -34,5 +34,30 @@ register_copy() {
   success "Package ${COLOR_GREEN}$category/$package${COLOR_RESET} registered"
 }
 
-register_copy "$(echo "$dirname" | sed 's/\/$//g')"
+unregister_copy () {
+  info "Unregistering a local copy of ${COLOR_GREEN}${dirname}${COLOR_RESET} at the formulas dir"
+
+  category=$(echo $1 | awk -F'/' '{print $(NF-1)}')
+  package=$(echo $1 | awk -F'/' '{print $NF}')
+
+  if ! test -d "/var/lib/hoshi/hoshi-formulas/$category/$package"; then
+    error "Package ${COLOR_GREEN}$category/$package${COLOR_RESET} does not exists"
+  fi
+
+  rm -rf "/var/lib/hoshi/hoshi-formulas/$category/$package"
+
+  if [[ "$(cat .gitignore | grep "$category/$package")" != "" ]]; then
+    sed -i "/$category\/$package/d" .gitignore
+  fi
+
+  success "Package ${COLOR_GREEN}$category/$package${COLOR_RESET} unregistered"
+}
+
+if [[ "$flag" == "register" || "$flag" == "" ]]; then
+  register_copy "$(echo "$dirname" | sed 's/\/$//g')"
+elif [[ "$flag" == "unregister" ]]; then
+  unregister_copy "$(echo "$dirname" | sed 's/\/$//g')"
+else
+  error "Invalid flag"
+fi
 
